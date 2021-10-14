8b58b67c41b28d31554276375674ee0f5ba9fd9c
Denis Arce <dnsarce21@gmail.com>
2021-10-13 21:53:37 -0500

    updated code

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require("path");
 const apiNotes = require("./Routes/apiNotes");
 const { fs } = require("fs");
-require('dotenv').config();
+// require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
